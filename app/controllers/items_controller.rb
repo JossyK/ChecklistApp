ed54c8085887f2c1bd3a8d2413c91e8411caebbd
jossy <yosef.kiros78@gmail.com>
2018-10-09 13:05:26 +0300

    First project Added into Git

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+	
+	before_action :find_item, only: [:show, :edit, :update, :destroy]
+	def index
+		if user_signed_in?
+		@item =Item.where(:user_id => current_user.id).order("created_at DESC")
+		end
+	end
+
+	def new
+		@item = current_user.items.build
+	end
+
+	def create
+		@item = current_user.items.build(params.require(:item).permit(:title, :description))
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end 
+
+	def show
+	end
+	
+	def edit
+	end
+
+	def update
+		if @item.update(item_params)
+			redirect_to item_path(@item)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to root_path
+	end
+
+	private
+		def find_item
+			@item = Item.find(params[:id])
+		end
+	# private
+	# def item_params
+	# 	params.require (:item).permit(:title, :description)
+	# end
+end 
+
